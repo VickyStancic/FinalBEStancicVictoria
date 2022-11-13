b5ab5b209db346836d11ed76fcd9863d00dc0ae6
VickyStancic <victoriastancic@hotmail.com>
2022-11-13 02:32:08 -0300

    ConfigServer

--- ConfigServer/src/main/java/com/dh/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/dh/ConfigServer/ConfigServerApplication.java
@@ -2,9 +2,10 @@ package com.dh.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EntableConfigServer
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
