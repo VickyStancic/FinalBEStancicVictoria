1e89b4291d6dc286137656da1ee9f5d6907a79ea
VickyStancic <victoriastancic@hotmail.com>
2022-11-14 04:31:51 -0300

    probando api-movie

--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -2,6 +2,8 @@ package com.dh.catalog.client;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @FeignClient(name="api-movie")
+@LoadBalancerClient(name = "api-movie", configuration = LoadBalancerClientConfiguration.class)
 public interface MovieServiceClient {
 	@GetMapping("/api/v1/movies/{genre}")
 	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
